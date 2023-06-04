07eccd5795ac21bc7d4592bcfd8fbc45367f3c2b
Huseyin Canbay <huseyincanbay10@gmail.com>
2023-06-04 16:26:13 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const db = require('./config/db.js');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cookieParser());
+
+db();
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
---
+++ server/models/Hotel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const hotelSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  photos: {
+    type: [String],
+  },
+  rooms: {
+    type: [String],
+  },
+  features: {
+    type: Boolean,
+    default: false,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Hotel = mongoose.model("Hotel", hotelSchema);
+
+module.exports = Hotel;
